425163100a97168ac415012a9410bd8e43fb01ee
Saaweel <sawelsports@gmail.com>
2023-05-30 18:08:15 +0200

    .

--- src/js/app.js
+++ src/js/app.js
@@ -62,6 +62,8 @@ function addCartItem(name, price, image) {
 
     let cartTotalHTML = document.getElementById("cart_total");
     cartTotalHTML.innerHTML = `Pagar: ${cart_total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} €`;
+    cartTotalHTML.disabled = false;
+
 }
 
 function deleteCartItem(id) {
@@ -75,6 +77,10 @@ function deleteCartItem(id) {
 
     let cartTotalHTML = document.getElementById("cart_total");
     cartTotalHTML.innerHTML = `Pagar: ${cart_total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} €`;
+
+    if (cart.length < 1) {
+        cartTotalHTML.disabled = true;
+    }
 }
 
 function getCartItemId() {
@@ -94,4 +100,126 @@ function toggleCart() {
             cartBoxs[i].style.display = "none";
         }
     }
+}
+
+function openCheckout() {
+    let checkout_total = document.getElementById("checkout_total");
+    checkout_total.innerHTML = `Pagar <span style="color: green;">${cart_total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} €</span>`;
+
+    let Checkout = document.getElementById("checkout");
+    Checkout.style.display = "block";
+}
+
+function closeCheckout() {
+    let Checkout = document.getElementById("checkout");
+    Checkout.style.display = "none";
+}
+
+function pay() {
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let country = document.getElementById("country").value;
+    let city = document.getElementById("city").value;
+    let postal_code = document.getElementById("postal_code").value;
+    let address = document.getElementById("address").value;
+
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let card_number = document.getElementById("card_number").value;
+    let card_cvv = document.getElementById("card_cvv").value;
+    let card_expiration = document.getElementById("card_expiration").value;
+
+    let result = document.getElementById("checkout_result");
+
+    if (email === "" || phone === "" || country === "" || city === "" || postal_code === "" || address === "" || name === "" || surname === "" || card_number === "" || card_cvv === "" || card_expiration === "") {
+        result.innerHTML = "Todos los campos son obligatorios";
+        result.style.display = "block";
+        result.style.color = "red";
+        return;
+    }
+
+    if (!email.includes("@") || !email.includes(".")) {
+        result.innerHTML = "El email no es válido";
+        result.style.display = "block";
+        result.style.color = "red";
+        return;
+    }
+
+    if (phone.length != 9) {
+        result.innerHTML = "El teléfono no es válido";
+        result.style.display = "block";
+        result.style.color = "red";
+        return;
+    }
+
+    if (postal_code.length != 5) {
+        result.innerHTML = "El código postal no es válido";
+        result.style.display = "block";
+        result.style.color = "red";
+        return;
+    }
+
+    if (card_number.length != 16) {
+        result.innerHTML = "El número de la tarjeta no es válido";
+        result.style.display = "block";
+        result.style.color = "red";
+        return;
+    }
+
+    if (card_cvv.length != 3) {
+        result.innerHTML = "El CVV no es válido";
+        result.style.display = "block";
+        result.style.color = "red";
+        return;
+    }
+
+    if (card_expiration.length != 7) {
+        result.innerHTML = "La fecha de caducidad no es válida";
+        result.style.display = "block";
+        result.style.color = "red";
+        return;
+    }
+
+    card_expiration = card_expiration.split("/");
+    card_expiration = card_expiration[1] + "-" + card_expiration[0] + "-01";
+    
+    if (isNaN(Date.parse(card_expiration))) {
+        result.innerHTML = "La fecha de caducidad no es válida";
+        result.style.display = "block";
+        result.style.color = "red";
+        return;
+    }
+
+    let date = new Date();
+    let card_expiration_date = new Date(card_expiration);
+
+    if (card_expiration_date < date) {
+        result.innerHTML = "La tarjeta está caducada";
+        result.style.display = "block";
+        result.style.color = "red";
+        return;
+    }
+
+    result.innerHTML = "Se está estableciendo la conexión con el banco...";
+    result.style.display = "block";
+    result.style.color = "blue";
+
+    setTimeout(() => {
+        result.innerHTML = "Pago realizado con éxito";
+        result.style.display = "block";
+        result.style.color = "green";
+        
+        setTimeout(() => {
+            result.innerHTML = "";
+            result.style.display = "none";
+            closeCheckout();
+            cart.splice(0, cart.length);
+            cart_total = 0;
+            let cartTotalHTML = document.getElementById("cart_total");
+            cartTotalHTML.innerHTML = `Pagar: ${cart_total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} €`;
+            cartTotalHTML.disabled = true;
+            let cartItems = document.getElementById("cart");
+            cartItems.innerHTML = "";
+        }, 1500);
+    }, 2500);
 }
